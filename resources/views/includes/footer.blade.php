231bde7042181f5e9587ecd5c469763d54d4d2c2
vishnu vs <54799446+vishnuvs369@users.noreply.github.com>
2021-12-14 22:28:00 +0530

    first commit

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,10 @@
+<!-- Footer -->
+<footer class="py-5 bg-dark">
+    <div class="container">
+        <p class="m-0 text-center text-white">
+            Copyright &copy; {{ config('app.name', 'Larablog') }} {{ now()->year }} 
+            | <a href="https://twitter.com/iamvishnuvs/">vishnuvs.com</a>
+        </p>
+    </div>
+    <!-- /.container -->
+</footer>
